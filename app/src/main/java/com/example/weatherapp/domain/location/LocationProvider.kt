73fdd38253e84857c204a159ffb67bd0d8b59a8e
Alireza <motlaghalireza943@gmail.com>
2026-09-16 23:46:09 +0330

    RebuildProjectClean

--- app/src/main/java/com/example/weatherapp/domain/location/LocationProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weatherapp.domain.location
-
-interface LocationProvider {
-    suspend fun getCurrentLocation(): LocationResult
-}
--- app/src/main/java/com/example/weatherapp/domain/location/LocationResult.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherapp.domain.location
-
-sealed interface LocationResult {
-    data class Success(val latitude: Double, val longitude: Double) : LocationResult
-    data object LocationDisabled : LocationResult
-    data object PermissionDenied : LocationResult
-    data object Unavailable : LocationResult
-}
